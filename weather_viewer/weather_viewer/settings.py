d9e341212b381ddc778006310452be3ea8ddeb5e
EgorFurman <egordedkov01@gmail.com>
2024-12-03 13:40:08 +0500

    Update settings

--- weather_viewer/weather_viewer/settings.py
+++ weather_viewer/weather_viewer/settings.py
@@ -188,10 +188,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'weather_viewer/static',
+    BASE_DIR / 'weather_viewer/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
